c7603365442160a17edb956e7fa84e81e8a80432
muzahid626 <muzahid626>
2022-10-22 22:31:19 +0600

    URL Mappings

--- Django_Practice_2/urls.py
+++ Django_Practice_2/urls.py
@@ -1,13 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-
-from first_app import views
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),
-    path('about/', views.about, name='about'),
+    path('customers/', include('first_app.urls')),
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>This is Homepage</h1> <a href="contact/">Contact</a> <a href="about/">About</a>')
+    return HttpResponse('<h1>I am From First Application</h1>')
 
 def contact(request):
-    return HttpResponse('<h1>This is Contact Page</h1> <a href="/">Home</a> <a href="/about/">About</a>')
+    return HttpResponse('<h1>This is Contact Page</h1>')
 
 def about(request):
-    return HttpResponse('<h1>This is About Page</h1> <a href="/">Home</a> <a href="/contact/">Contact</a>')
+    return HttpResponse('<h1>This is About Page</h1>')
