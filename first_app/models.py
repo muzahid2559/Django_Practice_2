d6928027c0877361aa1308311bab807d0a0dd73b
muzahid626 <muzahid626>
2022-10-25 13:52:00 +0600

    Creating Models

--- first_app/models.py
+++ first_app/models.py
@@ -2,15 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-# Treditional database(Sql code)
-CREATE TABLE Person (
-    "id" serial NOT NULL PRIMARY KEY,
-    "first_name" varchar(30) NOT NULL,
-    "last_name" varchar(30) NOT NULL
-);
-
 # django Models
-class Person(models.Model):
-    id = models.AutoField(primary_key=True)
+class Musician(models.Model):
+    # id = models.AutoField(primary_key=True) ...{hidden column auto make django}
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    instrument = models.CharField(max_length=30)
+
+
+class Album(models.Model):
+    # id = models.AutoField(primary_key=True) ...{hidden column auto make django}
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
